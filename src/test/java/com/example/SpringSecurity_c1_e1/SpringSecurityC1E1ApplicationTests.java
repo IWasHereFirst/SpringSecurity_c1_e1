5e130671ec605c43f37cd28a064fcfcf029bacd1
IWasHereFirst <umpiklumpik@gmail.com>
2023-01-16 09:32:26 +0100

    Init

---
+++ src/test/java/com/example/SpringSecurity_c1_e1/SpringSecurityC1E1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurity_c1_e1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityC1E1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
